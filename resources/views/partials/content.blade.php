e8610ded97a6ddc01f66109edc661fbeefda9d61
gabzon <gab.zambrano@gmail.com>
2018-09-27 12:38:14 +0200

    links menu

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -11,7 +11,7 @@ if ( get_post_meta(get_the_ID(),'ih-link', true) ) {
   <article @php post_class() @endphp>
     <br>
     <div class="row">
-      <div class="col-12 col-sm-3 col-md-4 col-lg-4">
+      <div class="col-12 col-sm-2 col-md-3 col-lg-3">
         <a class="no-underline black dim" href="{{ $link }}" target="_blank">
           @if( has_post_thumbnail() )
             <img src="{{get_the_post_thumbnail_url(get_the_ID(),'full')}}" class="img-fluid" alt="info-handicap-ge">
@@ -20,7 +20,7 @@ if ( get_post_meta(get_the_ID(),'ih-link', true) ) {
           @endif
         </a>
       </div>
-      <div class="col-12 col-sm-9 col-md-8 col-lg-8">
+      <div class="col-12 col-sm-10 col-md-9 col-lg-9">
         <a class="no-underline black dim" href="{{ $link }}" target="_blank">
           <h1 class="f3 fw1 dosis-bold mt0 lh-title">{{ get_the_title() }}</h1>
         </a>
