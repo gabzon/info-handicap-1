53fdbf24fda4db911b2cdc5af188eb5cd69018e3
gabzon <gab.zambrano@gmail.com>
2018-09-27 12:45:33 +0200

    image text smaller

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -11,7 +11,7 @@ if ( get_post_meta(get_the_ID(),'ih-link', true) ) {
   <article @php post_class() @endphp>
     <br>
     <div class="row">
-      <div class="col-12 col-sm-2 col-md-3 col-lg-3">
+      <div class="col-12 col-sm-1 col-md-2 col-lg-2">
         <a class="no-underline black dim" href="{{ $link }}" target="_blank">
           @if( has_post_thumbnail() )
             <img src="{{get_the_post_thumbnail_url(get_the_ID(),'full')}}" class="img-fluid" alt="info-handicap-ge">
@@ -20,9 +20,9 @@ if ( get_post_meta(get_the_ID(),'ih-link', true) ) {
           @endif
         </a>
       </div>
-      <div class="col-12 col-sm-10 col-md-9 col-lg-9">
+      <div class="col-12 col-sm-11 col-md-10 col-lg-10">
         <a class="no-underline black dim" href="{{ $link }}" target="_blank">
-          <h1 class="f3 fw1 dosis-bold mt0 lh-title">{{ get_the_title() }}</h1>
+          <h2 class="f4 fw1 dosis-bold mt0 lh-title">{{ get_the_title() }}</h2>
         </a>
         <p class="f6 f5-l lh-copy dosis">@php the_excerpt() @endphp</p>
         <p class="f6 lh-copy mv0">@include('partials/entry-meta')</p>
