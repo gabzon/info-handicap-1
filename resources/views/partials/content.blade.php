823b03553882619351b09a067e9444a27b013c0b
gabzon <gab.zambrano@gmail.com>
2018-10-23 11:35:34 +0200

    remove author

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -25,7 +25,6 @@ if ( get_post_meta(get_the_ID(),'ih-link', true) ) {
           <h2 class="f4 fw1 dosis-bold mt0 lh-title">{{ get_the_title() }}</h2>
         </a>
         <p class="f6 f5-l lh-copy dosis">@php the_excerpt() @endphp</p>
-        <p class="f6 lh-copy mv0">@include('partials/entry-meta')</p>
       </div>
     </div>
     <br>
